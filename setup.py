25dfe743163e35963d7c5776d387ca99853c4d20
zepoch <1654306546@qq.com>
2023-11-30 15:02:59 +0800

    '3dst'

--- setup.py
+++ setup.py
@@ -4,4 +4,19 @@ setup(
     name='3dst',
     packages=find_packages(),
     version = '0.0.1',
+    install_requires=[
+        'anndata',
+        'itertools',
+        'math',
+        'pyvista',
+        'numpy',
+        'pandas',
+        'k3d',
+        'scanpy',
+        'matplotlib',
+        'scipy',
+        'PyMCubes',
+        
+    ]
 )
+
